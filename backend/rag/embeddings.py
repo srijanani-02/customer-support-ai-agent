b02eab5e64a15daf04b8993e9b9e9ed95bd0f9f8
srijanani-02 <srijanani2526@gmail.com>
2026-07-07 22:44:17 +0530

    Optimized embedded loading for deployment

--- backend/rag/embeddings.py
+++ backend/rag/embeddings.py
@@ -1,20 +1,24 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
-
-print("Loading Embedding Model...")
-
-
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+_embedding_model = None
 
 
 def get_embedding_model():
+    global _embedding_model
 
-    return embedding_model
+    if _embedding_model is None:
+        print("Loading Embedding Model...")
+
+        _embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return _embedding_model
 
 
 if __name__ == "__main__":
+
+    embedding_model = get_embedding_model()
 
     vector = embedding_model.embed_query(
         "How do I reset my password?"
--- backend/rag/retriever.py
+++ backend/rag/retriever.py
@@ -1,35 +1,40 @@
 from langchain_chroma import Chroma
-
 from rag.embeddings import get_embedding_model
 
-
-print("Loading Embedding Model...")
-
-embedding_model = get_embedding_model()
-
-print("Opening ChromaDB...")
-
-vectorstore = Chroma(
-    persist_directory="chroma_db",
-    embedding_function=embedding_model
-)
-
-retriever = vectorstore.as_retriever(
-    search_kwargs={"k": 3}
-)
+_vectorstore = None
+_retriever = None
 
 
 def get_retriever():
+    global _vectorstore, _retriever
 
-    return retriever
+    if _retriever is None:
+
+        print("Loading Embedding Model...")
+
+        embedding_model = get_embedding_model()
+
+        print("Opening ChromaDB...")
+
+        _vectorstore = Chroma(
+            persist_directory="chroma_db",
+            embedding_function=embedding_model
+        )
+
+        _retriever = _vectorstore.as_retriever(
+            search_kwargs={"k": 3}
+        )
+
+    return _retriever
 
 
 if __name__ == "__main__":
+
+    retriever = get_retriever()
 
     docs = retriever.invoke(
         "How do I reset my password?"
     )
 
     for doc in docs:
-
         print(doc.page_content)
