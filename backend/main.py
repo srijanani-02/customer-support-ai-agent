a22456a5c104bb7ecf4f44db58be1e9b7af33a1a
Sri Janani P <srijanani2526@gmail.com>
2026-07-23 21:19:05 +0530

    Add new CORS origin for deployment

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://customer-support-ai-agent-nu.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
